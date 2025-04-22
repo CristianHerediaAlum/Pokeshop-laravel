05694f23efa2dfe49637ea42a95617d1ebb9d2e7
Warabaringo <cris.herediabravo@alum.uca.es>
2025-04-22 13:09:05 +0200

    agregado footer

---
+++ pokeshop/resources/views/layouts/app.blade.php
@@ -0,0 +1,24 @@
+<!-- resources/views/layouts/app.blade.php -->
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>@yield('title', 'Mi sitio')</title>
+    <link rel="icon" type="image/png" href="logo.png">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <link rel="stylesheet" href="{{ asset('css/estilos.css') }}">
+</head>
+<body>
+    <header>
+        <h1>Pokeshop</h1>
+    </header>
+
+    <main>
+        @yield('content')
+    </main>
+
+    @include('layouts.footer') <!-- Aquí se incluye el footer -->
+</body>
+</html>
---
+++ pokeshop/resources/views/pokeshop/index.blade.php
@@ -0,0 +1,9 @@
+<!-- resources/views/productos/index.blade.php -->
+@extends('layouts.app')
+
+@section('title', 'Pokeshop')
+
+@section('content')
+    <h2>Página principal</h2>
+    <!-- listado de productos -->
+@endsection
