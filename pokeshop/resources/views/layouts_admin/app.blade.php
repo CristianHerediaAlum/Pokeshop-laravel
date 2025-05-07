ba41f9a6599ebe9590ecb5f577bec0923643d24e
rubenmujiica <rubenmujicasanchez@gmail.com>
2025-05-07 22:26:05 +0200

    Cambios Fondo de la Web

--- pokeshop/resources/views/layouts_admin/app.blade.php
+++ pokeshop/resources/views/layouts_admin/app.blade.php
@@ -9,6 +9,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <link rel="stylesheet" href="{{ asset('css/estilos.css') }}">
+    <link rel="preload" as="image" href="{{ asset('imgs/fondo-optimizado.webp') }}" type="image/webp"> 
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     {{-- @vite('resources/css/app.css')
     @vite('resources/js/app.js') --}}
